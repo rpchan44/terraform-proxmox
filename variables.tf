c2d655b1017841b44d2b87ba010b1ca1a677097c
Ronaldo Chan <ronaldopchan@outlook.com>
2025-10-04 20:13:22 +0800

    Amend last commit (#7)

--- variables.tf
+++ variables.tf
@@ -7,33 +7,39 @@ variable "node_name" {
 variable "username" {
   type        = string
   description = "Username"
+  default     = "adminuser"
   sensitive   = true
 }
 
 variable "secret" {
   type        = string
   description = "secret"
+  default     = "adminuser"
   sensitive   = true
 }
 
 variable "disk_size" {
   type        = number
   description = "Disk Size"
+  default = 30
 }
 
 variable "memory" {
   type        = number
   description = "Memory Size"
+  default = 1024
 }
 
 variable "cpu_cores" {
   type        = number
   description = "Core Count"
+  default = 2
 }
 
 variable "gateway" {
   type        = string
   description = "Gateway"
+  default = "192.168.51.1"
 }
 
 
