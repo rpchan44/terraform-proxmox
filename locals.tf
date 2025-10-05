4bbd5b383bf27b976278c4fc1771493de699b644
Ronaldo Chan <ronaldopchan@outlook.com>
2025-10-05 11:05:12 +0800

    Reduce VM and change IP (#13)

--- locals.tf
+++ locals.tf
@@ -5,26 +5,14 @@ locals {
 
 locals {
   vms = {
-    "avq-test-01" = {
-      cidr     = "192.168.51.30/24"
-      os_image = "noble"
-      cores    = 1
-      ram      = 1024
-    }
-    "avq-test-02" = {
-      cidr     = "192.168.51.31/24"
-      os_image = "jammy"
-      cores    = 1
-      ram      = 1024
-    }
     "avq-test-03" = {
-      cidr     = "192.168.51.32/24"
+      cidr     = "192.168.51.31/24"
       os_image = "noble"
       cores    = 1
       ram      = 1024
     }
     "avq-test-04" = {
-      cidr     = "192.168.51.33/24"
+      cidr     = "192.168.51.32/24"
       os_image = "noble"
       cores    = 2
       ram      = 1024
