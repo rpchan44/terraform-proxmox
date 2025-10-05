c2d238ac2b1879aaf81d47875c215b26e4f753c1
Ronaldo Chan <ronaldopchan@outlook.com>
2025-10-05 13:43:19 +0800

    Update code

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "images" {
-  value = { for k, m in module.download_iso : k => m.image_id }
+  value = { for k, m in module.image_factory : k => m.image_id }
 }
 
 output "guest_name" {
-  value = [for m in module.launch_vm : m.vm_name]
+  value = [for m in module.launch_instance : m.vm_name]
 }
