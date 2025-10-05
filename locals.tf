0b17a42185212beaecd2e7dc54fec5e8cedc8561
Ronaldo Chan <ronaldopchan@outlook.com>
2025-10-05 18:02:08 +0800

    Reduce

--- locals.tf
+++ locals.tf
@@ -11,24 +11,24 @@ locals {
       cores    = 2
       ram      = 1024
     }
-    "vm-test-02" = {
-      cidr     = "192.168.51.32/24"
-      os_image = "noble"
-      cores    = 2
-      ram      = 1024
-    }
-    "vm-test-03" = {
-      cidr     = "192.168.51.33/24"
-      os_image = "noble"
-      cores    = 2
-      ram      = 1024
-    }
-    "vm-test-04" = {
-      cidr     = "192.168.51.34/24"
-      os_image = "noble"
-      cores    = 2
-      ram      = 1024
-    }
+    # "vm-test-02" = {
+    #   cidr     = "192.168.51.32/24"
+    #   os_image = "noble"
+    #   cores    = 2
+    #   ram      = 1024
+    # }
+    # "vm-test-03" = {
+    #   cidr     = "192.168.51.33/24"
+    #   os_image = "noble"
+    #   cores    = 2
+    #   ram      = 1024
+    # }
+    # "vm-test-04" = {
+    #   cidr     = "192.168.51.34/24"
+    #   os_image = "noble"
+    #   cores    = 2
+    #   ram      = 1024
+    # }
   }
 }
 
