906944fbaa69b1992f210faf0751f8823cb500ca
Ronaldo Chan <ronaldopchan@outlook.com>
2025-10-05 13:18:32 +0800

    Add and Resize

--- locals.tf
+++ locals.tf
@@ -9,13 +9,25 @@ locals {
       cidr     = "192.168.51.31/24"
       os_image = "noble"
       cores    = 2
-      ram      = 1024
+      ram      = 512
     }
     "avq-test-02" = {
       cidr     = "192.168.51.32/24"
       os_image = "noble"
       cores    = 2
-      ram      = 1024
+      ram      = 512
+    }
+    "avq-test-03" = {
+      cidr     = "192.168.51.33/24"
+      os_image = "noble"
+      cores    = 2
+      ram      = 512
+    }
+    "avq-test-04" = {
+      cidr     = "192.168.51.34/24"
+      os_image = "noble"
+      cores    = 2
+      ram      = 512
     }
   }
 }
