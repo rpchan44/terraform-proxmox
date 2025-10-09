c578bdae214a96a3494d650f3222cf10945a20e1
Ronaldo Chan <ronaldopchan@outlook.com>
2025-10-09 17:03:48 +0800

    Initial commit

---
+++ locals.tf
@@ -0,0 +1,40 @@
+locals {
+  env_config  = yamldecode(file("./env_config.yml"))
+  cloud_image = data.terraform_remote_state.installer.outputs.image_factory
+}
+
+locals {
+  vms = {
+    "vm-test-01" = {
+      cidr     = "192.168.51.31/24"
+      os_image = "noble"
+      cores    = 2
+      ram      = 1024
+      vlan     = 51       # this can be override default value set to null
+      #bridge  = "vmbr0"  # this can be override if you don't set it default to vmbr0
+      tags     = ["linux"]
+    }
+    "vm-test-02" = {
+      cidr     = "192.168.51.32/24"
+      os_image = "noble"
+      cores    = 2
+      ram      = 1024
+      vlan     = 51
+    }
+    # "vm-test-03" = {
+    #   cidr     = "192.168.51.33/24"
+    #   os_image = "noble"
+    #   cores    = 2
+    #   ram      = 1024
+    #   vlan     = 51
+    # }
+    # "vm-test-04" = {
+    #   cidr     = "192.168.51.34/24"
+    #   os_image = "noble"
+    #   cores    = 2
+    #   ram      = 1024
+    #   vlan     = 51
+    # }
+  }
+}
+
---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.84.1"
+    }
+  }
+}
+
+provider "proxmox" {
+
+  endpoint = var.pve_api
+  username = var.pve_user
+  password = var.pve_secret
+  insecure = true
+  ssh { agent = true }
+
+}
