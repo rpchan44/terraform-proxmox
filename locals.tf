9e04232eab8bcffbf0e4ccd9ae447a50aa7b0a76
Ronaldo Chan <ronaldopchan@outlook.com>
2025-10-18 13:08:36 +0800

    Restore

--- locals.tf
+++ locals.tf
@@ -37,28 +37,28 @@ locals {
       vlan     = 51
       tags     = ["linux","app2"]
     }
-    # "vm-test-03" = {
-    #   node     = "frodo"
-    #   dns      = ["1.1.1.1","8.8.8.8"]
-    #   cidr     = "192.168.51.33/24"
-    #   next-hop = "192.168.51.1"
-    #   os_image = "noble"
-    #   cores    = 2
-    #   ram      = 1024
-    #   vlan     = 51
-    #   tags     = ["linux","app3"]
-    # }
-    # "vm-test-04" = {
-    #   node     = "frodo"
-    #   dns      = ["1.1.1.1","8.8.8.8"]
-    #   cidr     = "192.168.51.34/24"
-    #   next-hop = "192.168.51.1"
-    #   os_image = "jammy"
-    #   cores    = 2
-    #   ram      = 1024
-    #   vlan     = 51
-    #   tags     = ["linux","app4"]
-    # }
+    "vm-test-03" = {
+      node     = "frodo"
+      dns      = ["1.1.1.1","8.8.8.8"]
+      cidr     = "192.168.51.33/24"
+      next-hop = "192.168.51.1"
+      os_image = "noble"
+      cores    = 2
+      ram      = 1024
+      vlan     = 51
+      tags     = ["linux","app3"]
+    }
+    "vm-test-04" = {
+      node     = "frodo"
+      dns      = ["1.1.1.1","8.8.8.8"]
+      cidr     = "192.168.51.34/24"
+      next-hop = "192.168.51.1"
+      os_image = "jammy"
+      cores    = 2
+      ram      = 1024
+      vlan     = 51
+      tags     = ["linux","app4"]
+    }
   }
 }
 
